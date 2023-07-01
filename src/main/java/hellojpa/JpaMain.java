72e0a6c81abb4d0d5f1f38a1fdff6227aa864936
Jaewoo Kim <wodndhksk@gmail.com>
2023-07-01 17:02:50 +0900

    영속성 컨텍스트 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,21 @@ public class JpaMain {
 //            insertMember(em);
 //            findMember(em);
 //            updateMember(em);
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
+//            findResultList(em);
 
-            for(Member member : resultList){
-                System.out.println("member.name = " + member.getName());
-            }
+//            entityManaged(em);
 
+            Member member = new Member();
+            //비영속
+            member.setId(102L);
+            member.setName("TestJpa");
+
+            //영속
+            em.persist(member);
+
+            // 영속성 컨텍스트의 1차 캐시에서 데이터 조회 (DB에서 조회 X)
+            Member findMember = em.find(Member.class, 102L);
+            System.out.println("findMember = "+ findMember.getName());
 
             tx.commit();
 
@@ -38,6 +46,32 @@ public class JpaMain {
 
         em.close();
         emf.close();
+    }
+
+    private static void entityManaged(EntityManager em) {
+        //비영속
+        Member member = em.find(Member.class, 1L);
+        member.setId(100L);
+        member.setName("TestJpa");
+
+        //영속
+        em.persist(member);
+
+        // 엔티티를 영속성 컨텍스트에서 분리해 준영속 상태로 만든다.
+//        em.detach(member);
+        // 영속성 콘텍스트를 비워도 관리되던 엔티티는 준영속 상태가 된다.
+//        em.claer();
+        // 영속성 콘텍스트를 종료해도 관리되던 엔티티는 준영속 상태가 된다.
+//        em.close();
+    }
+
+    private static void findResultList(EntityManager em) {
+        List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                .getResultList();
+
+        for (Member member : resultList) {
+            System.out.println("member.name = " + member.getName());
+        }
     }
 
     private static void updateMember(EntityManager em) {
