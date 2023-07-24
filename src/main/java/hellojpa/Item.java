a16016851f10c59a281f183a609036556ba04828
Jaewoo Kim <wodndhksk@gmail.com>
2023-07-24 22:38:30 +0900

    refactor: 주석 설명 추가
    - @DiscriminatorColumn (부모 클래스에 선언)
    - @DiscriminatorValue("M") (자식 클래스에 선언), DTYPE 구분값 "M"으로 세팅

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 //@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 부모 객체의 테이블 생성 X, 자식 객체(테이블)만 생성 및 부모 컬럼을 각각 중복으로 포함 [지양]
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 테이블 하나만 생성 [성능 향샹 및 테이블 단순화]
 @Inheritance(strategy = InheritanceType.JOINED) //테이블 여러개 생성, select 시 조인 [정석 방식]
-@DiscriminatorColumn
+@DiscriminatorColumn // 부모 클래스에 선언한다. 하위 클래스를 구분하는 용도의 컬럼(ex = DTYPE), name 설정 가능
 public abstract class Item { // 추상 클래스로 사용해야 TABLE_PER_CLASS 전략시 Item 테이블이 생성되지 않는다.
 
     @Id @GeneratedValue
--- src/main/java/hellojpa/Movie.java
+++ src/main/java/hellojpa/Movie.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Setter
-@DiscriminatorValue("M")
+@DiscriminatorValue("M") // 하위 클래스에 선언한다. 엔티티를 저장할 때 슈퍼타입의 구분 컬럼에 저장할 값을 지정한다.(save 시 부모의 DTYPE 컬럼은 = "M")
 public class Movie extends Item{
     private String director;
     private String actor;
